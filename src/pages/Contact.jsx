ca42e865764ff5270b1ef16063e415cf8debe96f
abdullah0saeed <abdullahhamouda056@gmail.com>
2025-03-11 20:26:02 +0200

    add button to send email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
 
 const myPhone = import.meta.env.VITE_MY_PHONE;
+const myEmail = import.meta.env.VITE_MY_EMAIL;
 
 const Contact = () => {
   const handelSubmit = (e) => {
@@ -24,6 +25,29 @@ const Contact = () => {
     e.target.reset();
   };
 
+  const handelEmail = (e) => {
+    e.preventDefault();
+
+    const name = e.target.form.elements.name.value;
+    const email = e.target.form.elements.email.value;
+    const message = e.target.form.elements.message.value;
+
+    // Construct the Email URL
+    const emailUrl = `mailto:
+      ${myEmail}?subject=${encodeURIComponent(
+      "Contact Form Submission from Portfolio"
+    )}&body=${encodeURIComponent(
+      `${name ? `Name: ${name}\n` : ""}${
+        email ? `Email: ${email}\n` : ""
+      }Message: ${decodeURIComponent(message)}`
+    )}`;
+
+    // Redirect to Email
+    window.open(emailUrl, "_blank"); // Open Email in a new tab
+
+    e.target.reset();
+  };
+
   return (
     <section id="contact" className="py-5">
       <Container>
@@ -33,23 +57,41 @@ const Contact = () => {
         <Form onSubmit={(e) => handelSubmit(e)}>
           <Form.Group className="mb-3 fade-in">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter your name" />
+            <Form.Control
+              name="name"
+              type="text"
+              placeholder="Enter your name"
+            />
           </Form.Group>
           <Form.Group className="mb-3 fade-in">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter your email" />
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+            />
           </Form.Group>
           <Form.Group className="mb-3 fade-in">
             <Form.Label>Message</Form.Label>
             <Form.Control
+              name="message"
               as="textarea"
               rows={3}
               placeholder="Enter your message"
             />
           </Form.Group>
-          <Button variant="success" type="submit" className="fw-bold">
-            <i className="bi bi-whatsapp me-2"></i> Send Message
-          </Button>
+          <Form.Group className="mb-3 fade-in justify-content-between d-flex">
+            <Button variant="success" type="submit" className="fw-bold">
+              <i className="bi bi-whatsapp me-2"></i> Send Message
+            </Button>
+            <Button
+              variant="dark"
+              className="fw-bold"
+              onClick={(e) => handelEmail(e)}
+            >
+              <i class="bi bi-envelope-arrow-up-fill me-2"></i> Send Email
+            </Button>
+          </Form.Group>
         </Form>
       </Container>
     </section>
